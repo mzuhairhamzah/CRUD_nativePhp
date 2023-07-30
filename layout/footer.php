23eaba70370debe14456b5cda831428af892e592
zuhair-hamzah <zuhair.hamzah08@gmail.com>
2023-07-30 19:12:59 +0800

    first commit

---
+++ layout/footer.php
@@ -0,0 +1,18 @@
+<!-- Option 1: Bootstrap Bundle with Popper -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
+</script>
+<!-- asset plugin data-tables -->
+<script src="https://code.jquery.com/jquery-3.7.0.js"></script>
+<script src="https://cdn.datatables.net/1.13.5/js/jquery.dataTables.min.js"></script>
+<script src="https://cdn.datatables.net/1.13.5/js/dataTables.bootstrap5.min.js"></script>
+
+<script>
+$(document).ready(function() {
+    $('#table').DataTable();
+});
+// new DataTable('#table');
+</script>
+</body>
+
+</html>
